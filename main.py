f013113e25735b29aacd2fb5c0edad156c3cba00
HunterCartier702 <huntercartier2@hotmail.com>
2025-06-03 11:46:29 -0700

    Updates from Thinkpad

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from stats import get_book_wc
+from stats import get_book_text, get_book_cc, get_book_wc
 
 
 def main():
-	return get_book_text("books/frankenstein.txt")
+	text = get_book_text("books/frankenstein.txt")
+	word_count = get_book_wc(text)
+	print(word_count)
+	
+	chars = get_book_cc(text)
+	print(chars)
+	# for key, value in chars.items():
+	# 	print(f"{key}: {value}")
 
+	
 
-#print(main())
-print(get_book_wc())
+if __name__ == '__main__':
+	main()
+# print(main())
--- stats.py
+++ stats.py
@@ -1,12 +1,26 @@
+# read text of book
 def get_book_text(file_path):
 	with open(file_path, 'r', encoding='utf-8') as f:
 		file_contents = f.read()
 	return file_contents
 
-def get_book_wc():
+
+# get word count of book
+def get_book_wc(text):
 	word_count = 0
-	wc = get_book_text("books/frankenstein.txt")
-	words = wc.split()
+	words = text.split()
 	for word in words:
 		word_count += 1
 	return f"{word_count} words found in the document"
+
+
+# get character count of book
+def get_book_cc(text):
+	text = text.lower()
+	char_count = {}
+	for char in text:
+		if char in char_count:
+			char_count[char] += 1
+		else:
+			char_count[char] = 1
+	return char_count
